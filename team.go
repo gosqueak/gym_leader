f6ed0eff153ae7902387e5320f9d3114eb07a3f2
rebeljah <rebeljah@gmail.com>
2023-05-11 00:47:54 -0400

    basic modeling

--- team.go
+++ team.go
@@ -1,1 +1,29 @@
 package leader
+
+type jwtAudience struct{}
+
+type serviceEndpoint struct {
+	Url     string
+	Methods map[string]bool
+}
+
+type serviceNodeAttributes struct {
+	Url         string
+	JWTAudience jwtAudience
+	Endpoints   []serviceEndpoint
+}
+
+type ServiceNode struct {
+	Dependents   map[*ServiceNode]bool
+	Dependencies map[*ServiceNode]bool
+	Attrs        serviceNodeAttributes
+}
+
+func (s *ServiceNode) Uses(other *ServiceNode) {
+	s.Dependencies[other] = true
+	other.usedBy(s)
+}
+
+func (s *ServiceNode) usedBy(other *ServiceNode) {
+	s.Dependents[other] = true
+}
