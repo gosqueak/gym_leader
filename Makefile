1eafb11d5eaf741121f6c7976ff6179bc6a4d9c1
rebeljah <rebeljah@gmail.com>
2023-05-12 00:14:30 -0400

    added basic CLI

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
+download_json:
+	go run . download https://raw.githubusercontent.com/gosqueak/leader/main/Teamfile.json ./Teamfile.json
+
 export_json:
-	go run .
+	go run . export ./Teamfile ./Teamfile.json
 
 git_push:
 	git checkout main
--- main.go
+++ main.go
@@ -1,8 +1,29 @@
 package main
 
-import "github.com/gosqueak/leader/team"
+import (
+	"fmt"
+	"os"
+
+	"github.com/gosqueak/leader/team"
+)
 
 func main() {
-	team := team.Load("Teamfile")
-	team.SaveJSON("Teamfile.json")
+	mode := os.Args[1]
+
+	switch mode {
+	case "download":
+		url := os.Args[2]
+		fp := os.Args[3]
+
+		fmt.Printf("downloading teamfile json from %v to %v\n", url, fp)
+		tm := team.Download(url)
+		tm.SaveJSON(fp)
+	
+	case "export":
+		teamfileFP := os.Args[2]
+		jsonFileFP := os.Args[3]
+
+		tm := team.Load(teamfileFP)
+		tm.SaveJSON(jsonFileFP)
+	}
 }
